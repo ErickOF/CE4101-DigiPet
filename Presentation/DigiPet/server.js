3fba65b9a355b58bca7908b49e603027af429eee
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2026-04-08 01:56:11 -0600

    [Update] Updating to NodeJS 25.9 and Angular 21.2.6 for WebPage

--- Presentation/DigiPet/server.js
+++ Presentation/DigiPet/server.js
@@ -5,8 +5,8 @@ const path = require('path');
 
 app.use(express.static(__dirname + '/dist/DigiPet'));
 app.listen(process.env.PORT || 8080);
-app.get('*', function(req, res) {
-	resp.sendFile(path.join(__dirname + '/dist/DigiPet/index.html'));
+app.get(/.*/, function(req, res) {
+	res.sendFile(path.join(__dirname + '/dist/DigiPet/index.html'));
 });
 
 console.log('Listenting!');
